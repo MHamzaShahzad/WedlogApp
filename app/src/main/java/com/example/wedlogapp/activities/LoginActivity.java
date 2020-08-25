6d6597a33029da9377b6dbf0ab8e2a9c48f0ee2e
MHamzaShahzad <hamzashahzad604@gmail.com>
2020-08-25 12:40:45 +0500

    Splash, Login and Register Account - blanks activities added.

--- app/src/main/java/com/example/wedlogapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/wedlogapp/activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.wedlogapp;
+package com.example.wedlogapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.wedlogapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/wedlogapp/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.wedlogapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wedlogapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
