5956758fc277b2ec32aaf37cf72991f8ee2bb514
Hamza Shahzad <MHamzaShahzad@users.noreply.github.com>
2020-09-07 09:42:18 +0500

    Missing files uploaded.

--- app/src/main/java/com/example/wedlogapp/adapters/hall_orders/HallOrdersAdapterHolder.java
+++ app/src/main/java/com/example/wedlogapp/adapters/hall_orders/HallOrdersAdapterHolder.java
@@ -1,12 +1,23 @@
 package com.example.wedlogapp.adapters.hall_orders;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wedlogapp.R;
+
 public class HallOrdersAdapterHolder extends RecyclerView.ViewHolder {
+
+    private TextView textPlaceName, textPlaceAppointDate, textPlaceSeatingPlan, textPlaceCustomNote;
+
     public HallOrdersAdapterHolder(@NonNull View itemView) {
         super(itemView);
+
+        textPlaceName = itemView.findViewById(R.id.textPlaceName);
+        textPlaceAppointDate = itemView.findViewById(R.id.textPlaceAppointDate);
+        textPlaceSeatingPlan = itemView.findViewById(R.id.textPlaceSeatingPlan);
+        textPlaceCustomNote = itemView.findViewById(R.id.textPlaceCustomNote);
     }
 }
